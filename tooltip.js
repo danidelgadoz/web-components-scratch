2f32e72f35ab22fd02d50f2ac8d9d0dca3596a3d
Daniel Delgado <dedd1993@gmail.com>
2019-12-31 11:43:08 -0500

    Interacting with the Surrounding DOM

--- tooltip.js
+++ tooltip.js
@@ -2,6 +2,9 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         console.log('Tooltip::constructor');
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.textContent = ' (?)';
+        this.appendChild(tooltipIcon);
         
     }
 }
