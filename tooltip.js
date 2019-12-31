0d49c5b810c974e2f833de6c8991053bf3d76c86
Daniel Delgado <dedd1993@gmail.com>
2019-12-31 11:48:33 -0500

    Using "connectedcallback" for DOM Access

--- tooltip.js
+++ tooltip.js
@@ -2,10 +2,14 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         console.log('Tooltip::constructor');
+        
+    }
+    
+    connectedCallback() {
+        console.log('Tooltip::connectedCallback');
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
         this.appendChild(tooltipIcon);
-        
     }
 }
 
