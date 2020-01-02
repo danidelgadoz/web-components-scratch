d66bac17d711f6dc1f0d5bf2446c87803029dfb6
Daniel Delgado <dedd1993@gmail.com>
2020-01-01 21:41:11 -0500

    Styling "slot" Content Outside of the Shadow DOM

--- confirm-link.js
+++ confirm-link.js
@@ -2,7 +2,7 @@ class ConfirmLink extends HTMLAnchorElement {
 
     connectedCallback() {
         this.addEventListener('click', event => {
-            if (!confirm('Do you really wwant to leave?')) {
+            if (! confirm('Do you really wwant to leave?')) {
                 event.preventDefault();
             }
         });
